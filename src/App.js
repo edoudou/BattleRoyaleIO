c90655b0ccf9ced35624a462ce7d28cec37f77b0
Quentin Cabanes <cabanes@ece.fr>
2018-03-30 16:36:04 +0200

    Add character movement, animation and camera

--- src/App.js
+++ src/App.js
@@ -7,21 +7,28 @@ import Character from "./core/Character.js";
 import Input from "./core/Input.js";
 import KeyboardInput from "./input/KeyboardInput.js";
 
-import playerSprite from './ressources/player.png';
-import tile0 from './ressources/tile0.png';
-
 const app = new PIXI.Application();
 
-let state;
-let player = {};
-let keys;
-let loadedBoard = {};
+let player;
+let map;
 
-let board =[[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0]];
+let board =[
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+];
 
 class App extends Component {
     constructor(props) {
@@ -44,78 +51,37 @@ class App extends Component {
         app.renderer.autoResize = true;
         app.renderer.resize(window.innerWidth, window.innerHeight);
         
-        let m = new Map(app);
-        let c = new Character(app);
+        map = new Map(app);
+        player = new Character(app.stage);
         let k = new KeyboardInput();
 
-        m.setTiles(board);
-        c.setInput(k);
+        map.setTiles(board);
+        player.setInput(k);
         
         PIXI.loader
-            .add(m.getTileset())
-            .add(c.getTileset())
+            .add(map.getTileset())
+            .add(player.getTileset())
             .load(function(loader, resources) {
-                m.setup();
-                c.setup();
+                map.setup();
+                player.setup();
             });
         
         
-        /*PIXI.loader .add('player', playerSprite)
-        .add('tile0', tile0)
+        PIXI.loader
         .load((loader, resources) => {
-            player.pixi = new PIXI.Sprite(resources.player.texture);
-            
-            // Setup the position
-            player.pixi.x = app.renderer.width / 2;
-            player.pixi.y = app.renderer.height / 2;
-            player.vx = 0;
-            player.vy = 0;
-            player.x = 250;
-            player.y = 250;
-            
-            // Rotate around the center
-            player.pixi.anchor.x = 0.5;
-            player.pixi.anchor.y = 0.5;
-            
-            // Add to the scene
-            app.stage.addChild(player.pixi);
-            
-            
-            keys = {};
-            
-            keys.right = this.keyboard(39);
-            keys.right.press = () => {player.vx +=5};
-            keys.right.release  = () => {player.vx -=5};
-            
-            keys.left = this.keyboard(37);
-            keys.left.press = () => {player.vx -=5};
-            keys.left.release  = () => {player.vx +=5};
-            
-            keys.down = this.keyboard(40);
-            keys.down.press = () => {player.vy +=5};
-            keys.down.release  = () => {player.vy -=5};
-            
-            keys.up = this.keyboard(38);
-            keys.up.press = () => {player.vy -=5};
-            keys.up.release  = () => {player.vy +=5};
-            
             // Listen for frame updates
             app.ticker.add(delta => this.gameLoop(delta));
-        });*/
+        });
     }
     
     gameLoop = (delta) => {
-        if(0 <= player.x + (player.vx * delta) && player.x + (player.vx * delta) <= 500)player.x += player.vx * delta;
-        if(0 <= player.y + (player.vy * delta) && player.y + (player.vy * delta) <= 500)player.y += player.vy * delta;
+        player.loop(delta);
         
-        //loadedBoard.pixi.x = player.pixi.x - player.x;
-        //loadedBoard.pixi.y = player.pixi.y - player.y;
+        //let dx = app.renderer.plugins.interaction.mouse.global.x - app.renderer.width / 2;
+        //let dy = app.renderer.plugins.interaction.mouse.global.y - app.renderer.height / 2;
+        //let theta = Math.atan2(dy, dx);
         
-        let dx = app.renderer.plugins.interaction.mouse.global.x - app.renderer.width / 2;
-        let dy = app.renderer.plugins.interaction.mouse.global.y - app.renderer.height / 2;
-        let theta = Math.atan2(dy, dx);
-        
-        player.pixi.rotation = theta;
+        //player.pixi.rotation = theta;
     }
     
     keyboard = (keyCode) => {
--- src/core/Character.js
+++ src/core/Character.js
@@ -4,29 +4,25 @@ import tileset from '../ressources/tileset_character.png';
 
 class Character {
     sprite = {width: 64, height: 64};
-    x = 0;
-    y = 0;
-    width = this.sprite.width;
-    height = this.sprite.height;
+    position = {x: 100, y: 100};
+    speed = {x: 0, y: 0};
+    anchor = {x: 0.5, y: 0.5};
+    size = {width: this.sprite.width, height: this.sprite.height };
+    direction = 0;
+
+    update_display = 0;
 
     // [START] Testing, to be removed
     pos = 0
     max_pos = 4;
     // [END]
 
-    constructor(app) {
-        this.app = app;
+    constructor(camera) {
+        this.camera = camera;
     }
 
     getTileset() {
         return tileset;
-    }
-
-    move(_this) {
-        _this.pos = (_this.pos + 1) % _this.max_pos;
-        let rectangle = new PIXI.Rectangle(_this.pos * _this.sprite.width, 0, _this.sprite.width, _this.sprite.height);
-        _this.texture.frame = rectangle
-        _this.display = new PIXI.Sprite(_this.texture);
     }
 
     setup() {
@@ -35,16 +31,73 @@ class Character {
 
         this.texture.frame = rectangle;
         let char = new PIXI.Sprite(this.texture);
+        char.x = this.position.x;
+        char.y = this.position.y;
+        char.anchor.x = this.anchor.x;
+        char.anchor.y = this.anchor.y;
 
-        this.display = this.app.stage.addChild(char);
+        this.display = this.camera.addChild(char);
+        
     }
 
     setInput(input) {
         if (!(input instanceof Input))
             return;
 
+        const _this = this;
+
         this.input = input;
-        input.bind("DOWN_PRESS", this.move, this);
+
+        input.bind("UP_PRESS", () => { _this.speed.y -= 5; });
+        input.bind("UP_RELEASE", () => { _this.speed.y += 5; });
+
+        input.bind("DOWN_PRESS", () => { _this.speed.y += 5; });
+        input.bind("DOWN_RELEASE", () => { _this.speed.y -= 5; });
+
+        input.bind("LEFT_PRESS", () => { _this.speed.x += 5; });
+        input.bind("LEFT_RELEASE", () => { _this.speed.x -= 5; });
+
+        input.bind("RIGHT_PRESS", () => { _this.speed.x -= 5; });
+        input.bind("RIGHT_RELEASE", () => { _this.speed.x += 5; });
+    }
+
+    loop(delta) {
+        this.position.x += this.speed.x * delta;
+        this.position.y += this.speed.y * delta;
+        
+        this.draw(delta);
+    }
+
+    draw(delta) {
+        this.update_display += delta;
+
+        if (this.update_display > 5) {
+            this.update_display = 0;
+
+            if (this.speed.x != 0 || this.speed.y != 0) {
+                this.updateDirection();
+                this.pos = (this.pos + 1) % this.max_pos;
+                let rectangle = new PIXI.Rectangle(this.pos * this.sprite.width, this.direction * this.sprite.height, this.sprite.width, this.sprite.height);
+                this.texture.frame = rectangle
+            }
+        }
+
+        this.display.x = this.position.x;
+        this.display.y = this.position.y;
+
+        this.camera.pivot.x = this.position.x - window.innerWidth/2;
+        this.camera.pivot.y = this.position.y - window.innerHeight/2;
+    }
+
+    updateDirection() {
+        if (this.speed.y > 0)
+            this.direction = 0;
+        else if (this.speed.x < 0)
+            this.direction = 1;
+        else if (this.speed.x > 0)
+            this.direction = 2;
+        else if (this.speed.y < 0)
+            this.direction = 3;
     }
 }
 
